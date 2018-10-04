3227e10108bd272fd7d8eabca41d071850fdc51c
Lucas Arthur <lucas.felgueiras@usp.br>
2018-10-04 18:14:45 -0300

    customize devise layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def highlight_page_on_navigation(current_path)
+    if current_page?(current_path)
+      "nav-link active"
+    else
+      "nav-link"
+    end
+  end
+
+  def translate_relations_name(objects)
+    objects&.klass&.model_name&.human(count: objects.length)
+  end
+
+  def translate_model_name(object)
+    object&.model_name&.human
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :devices
   devise_for :users
+
+  resources :devices
   resources :users
+
+  get '/', to: 'home#index', as: 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
